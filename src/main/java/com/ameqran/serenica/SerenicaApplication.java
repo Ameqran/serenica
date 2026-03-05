63c632a6e809f0520d4b3c4475eab6b7450d61a6
Ameqran <marouaneboukedir@gmail.com>
2026-03-05 13:01:36 +0100

    chore: scaffold spring boot backend with postgres compose

---
+++ src/main/java/com/ameqran/serenica/SerenicaApplication.java
@@ -0,0 +1,12 @@
+package com.ameqran.serenica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerenicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerenicaApplication.class, args);
+    }
+}
